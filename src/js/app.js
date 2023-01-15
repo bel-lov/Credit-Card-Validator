4314bdf33c029cb4737fc2083d99f476040dfafd
bel-lov <belou.lyuba@yandex.ru>
2023-01-15 22:47:15 +0300

    исправление ошибок

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,26 @@ import validCards from "./validCards";
 import validate from "./validators";
 
 if (typeof document !== "undefined") {
-    const input = document.querySelector(".form-control");
+  const input = document.querySelector(".form-control");
 
-    input.addEventListener("input", () => {
-        const type = validCards(input.value);
-        const cards = document.querySelectorAll('.card');
-        cards.forEach(item => {
-            item.classList.remove('active')
-        });
-        if (type) {
-            document.querySelector(type).classList.add('active')
-        }
+  input.addEventListener("input", () => {
+    const type = validCards(input.value);
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((item) => {
+      item.classList.remove("active");
     });
+    if (type) {
+      document.querySelector(type).classList.add("active");
+    }
+  });
 
-    input.parentNode.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const result = document.getElementById("result");
-        result.textContent = "";
-        validate(input.value)
-            ? (result.textContent = "Действующая карта")
-            : (result.textContent = "Введён некорректный номер карты!");
-        document.getElementById("card_number").value = " ";
-    });
+  input.parentNode.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const result = document.getElementById("result");
+    result.textContent = "";
+    validate(input.value)
+      ? (result.textContent = "Действующая карта")
+      : (result.textContent = "Введён некорректный номер карты!");
+    document.getElementById("card_number").value = " ";
+  });
 }
