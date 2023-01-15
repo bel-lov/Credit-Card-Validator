682de4e91a08267dc5d9e3af30b614eb72a01292
bel-lov <belou.lyuba@yandex.ru>
2023-01-15 21:23:40 +0300

    иконки исчезают и поле ввода очищается

--- src/js/app.js
+++ src/js/app.js
@@ -2,22 +2,26 @@ import validCards from "./validCards";
 import validate from "./validators";
 
 if (typeof document !== "undefined") {
-  const input = document.querySelector(".form-control");
+    const input = document.querySelector(".form-control");
 
-  input.addEventListener("input", () => {
-    const type = validCards(input.value);
+    input.addEventListener("input", () => {
+        const type = validCards(input.value);
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(item => {
+            item.classList.remove('active')
+        });
+        if (type) {
+            document.querySelector(type).classList.add('active')
+        }
+    });
 
-    if (type) {
-      document.querySelector(type).style.opacity = 1;
-    }
-  });
-
-  input.parentNode.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const result = document.getElementById("result");
-    result.textContent = "";
-    validate(input.value)
-      ? (result.textContent = "Действующая карта")
-      : (result.textContent = "Введён некорректный номер карты!");
-  });
+    input.parentNode.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const result = document.getElementById("result");
+        result.textContent = "";
+        validate(input.value)
+            ? (result.textContent = "Действующая карта")
+            : (result.textContent = "Введён некорректный номер карты!");
+        document.getElementById("card_number").value = " ";
+    });
 }
--- src/js/validCards.js
+++ src/js/validCards.js
@@ -1,21 +1,21 @@
 export default function validCards(value) {
-  if (/^4\d{12}(?:\d{3})/.test(value)) {
-    return ".visa";
-  } else if (/(^5[1-5])\d{14}/.test(value)) {
-    return ".master";
-  } else if (/(^3[47])\d{13}/.test(value)) {
-    return ".amex";
-  } else if (/^(?:2131|1800|35\d{3})\d{11}/.test(value)) {
-    return ".jcb";
-  } else if (/(^2||6)\d{15}/.test(value)) {
-    return ".mir";
-  } else if (
-    /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})/.test(
-      value
-    )
-  ) {
-    return ".discover";
-  }
+    if (/^4\d{12}(?:\d{3})/.test(value)) {
+        return ".visa";
+    } else if (/(^5[1-5])\d{14}/.test(value)) {
+        return ".master";
+    } else if (/(^3[47])\d{13}/.test(value)) {
+        return ".amex";
+    } else if (/^(?:2131|1800|35\d{3})\d{11}/.test(value)) {
+        return ".jcb";
+    } else if (/(^2||6)\d{15}/.test(value)) {
+        return ".mir";
+    } else if (
+        /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})/.test(
+            value
+        )
+    ) {
+        return ".discover";
+    }
 
-  return null;
+    return null;
 }
